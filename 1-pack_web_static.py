110be29c537d3a5227490ce42b9ba1243ec063bd
fredjuma007 <fredjuma007>
2023-04-05 22:12:41 +0200

    not yet testet though

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
-Usage: fab -f 1-pack_web_static.py do_pack
-All files in the folder web_static must be added to the final archive
-All archives must be stored in the folder 'versions' (create folder if none)
-Create archive "web_static_<year><month><day><hour><minute><second>.tgz"
-The function do_pack must return the archive path, else return None
-"""
+# makes tgz archive
 from fabric.api import local
 from time import strftime
 
@@ -21,3 +14,4 @@ def do_pack():
         return filename
     except:
         return None
+
