79eb153dd3824fe05aef5e49d8bb17ac9858d819
fredjuma007 <fredjuma007>
2023-04-06 22:11:53 +0200

    trying another script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 # makes tgz archive
+import tarfile
 from fabric.api import local
 from datetime import datetime
+import os
 
-from fabric.decorators import runs_once
 
-
-@runs_once
 def do_pack():
-
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
+    # packs file
+    try:
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S")
+        local('mkdir -p versions')
+        local("tar -cvzf versions/{}.tgz {}".format(
+            name, "web_static/"))
+        size = os.path.getsize("./versions/{}.tgz".format(name))
+        print("web_static packed: versions/{}.tgz -> {}Bytes".format(
+            name, size))
+    except:
         return None
-    return path
+
